aa932f0304d296ee5ecedb4c8ccb590d8cac48a4
Matthaios9 <73692514+Matthaios9@users.noreply.github.com>
2023-09-08 13:37:24 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { useEffect,useContext } from 'react';
+import { useState } from 'react';
+import { Navbar, Nav, Button ,NavDropdown, Form, FormControl } from 'react-bootstrap';
+import  { addToDatabaseCart, getDatabaseCart, removeFromDatabaseCart, processOrder } from './databaseManager';
+import {RoomContext} from '../App';
+import {UserContext} from '../App';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import './style.css';
+
+
+const Header = () => {
+    const [total,setTotal]=useState(0)
+    const [flag,setFlag]=useState(false);
+    const [bookedRoom,setBookedRoom]=useContext(RoomContext);
+
+    const [user,setUser]=useContext(UserContext);
+
+    if(user.state){
+        console.log(user.state);
+         
+    }
+
+    const logo="https://i.ibb.co/djHMH3K/pngegg.png";
+     
+    
+
+    return (
+        <Navbar bg="primary" expand="lg" sticky="top">
+            <div className="container">
+                <Navbar.Brand class="remove">
+                    <Link style={{textDecoration:"inherit"}} to="/home">
+                    <img class="d-block mx-auto" src="https://i.ibb.co/jR5LcWJ/kisspng-hotel-computer-icons-symbol-high-resolution-india-map-5b5cb6413113c7-360607041532802625201.png" style={{width:"30px"}} alt=""/>
+                    
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto" style={{color:"black"}}>
+                            <Nav.Link> <Link className="remove" to="/home">
+                                <span class="nav-custom">Home</span>
+                            </Link></Nav.Link>
+                            
+                            
+                            <Nav.Link><Link className="remove" to="/rooms">Rooms</Link>  </Nav.Link>
+                            <Nav.Link><Link className="remove" to="/booked">Current Bookings</Link></Nav.Link>
+                            {
+                                !user.state && <Nav.Link > <Link className="remove" to="/login">Sign In</Link> </Nav.Link>
+                            }
+                            {
+                                !user.state && <Nav.Link ><Link className="remove" to="/signup">Sign Up</Link></Nav.Link>
+                            }
+                            
+                            {
+                                user.state && <Nav.Link className="remove" onClick={()=>{
+                                    var info={
+                                        name:"",
+                                        email:"",
+                                        state:false
+                                    }
+                                    setUser(info);
+                                }}>Sign Out</Nav.Link>
+                            }
+                        
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="https://zutet.com/" target="_blank" className="remove">Portfolio</Nav.Link>
+                            <Nav.Link href="https://github.com/Matthaios9" target="_blank" className="remove">Github</Nav.Link>
+                        </Nav>
+                     
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react';
+
+import './style.css';
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch,
+  } from 'react-router-dom';
+ 
+
+const Home = () => {
+    useEffect(() => {
+        window.scrollTo(0,0);
+    },[])
+    const url="https://i.ibb.co/84CQx44/kisspng-hotel-manager-child-apps-hotel-illustration-hotels-5a7a4b87c57cd2-0897105415179641678089.png"
+    return (
+        <div className="container">
+            <div className="row justify-content-center align-items-center ">
+            <div className="col-lg-6  col-md-12 col-12">
+                <h1>HOTEL NIRVANA</h1>
+                <h5 className="h5-banner">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ad.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ad.
+                </h5>
+            </div>
+            <div className="col-lg-6 col-md-12 col-12">
+                <img src={url} className="text-center" style={{width:"90%"}} alt=""/>
+            </div>
+        </div>
+        <Link style={{textDecoration:"inherit"}} to="/rooms">
+        <button  className="btn btn-secondary d-block mx-auto">
+            <span style={{textDecoration:"inherit",color:"white"}}>Check Our Room & Services</span>
+             </button></Link>
+        </div>
+        
+    );
+};
+
+export default Home;
